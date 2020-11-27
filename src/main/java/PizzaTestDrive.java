921a4b4a48fa2c3fb18082892b673496c9dc3ad3
yassine.lakhal <yassine.lakhal.mehdi@gmail.com>
2020-11-27 17:58:45 +0100

    fix #All

--- src/main/java/PizzaTestDrive.java
+++ src/main/java/PizzaTestDrive.java
@@ -10,7 +10,6 @@ public class PizzaTestDrive {
 
         Pizza pizza = boutiqueBrest.commanderPizza("fromage");
         System.out.println("JMB a commandé une " + pizza.getNom() + "\n");
-
         pizza = boutiqueStrasbourg.commanderPizza("fromage");
         System.out.println("JMI a commandé une " + pizza.getNom() + "\n");
     }
